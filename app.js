9253663f1494804e33ec5710871e051782a731e9
Rohan <rohanchoudhary1898@gmail.com>
2020-07-30 18:03:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Selectors
+const newTask= document.getElementById('new-task');
+const filterTask=document.getElementById('filter-task');
+const addTask=document.querySelector('.btn');
+const clearTask=document.querySelector('.clear-btn')
+
+// Event Listeners
+
